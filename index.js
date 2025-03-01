b939d2965b829e055e6800d271f558653ddaf513
조성진 <lmsj122@gachon.ac.kr>
2025-03-01 20:32:22 +0900

    getObject function fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const sharp = require('sharp');
-const { S3Client } = require('@aws-sdk/client-s3');
+const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
 
 const s3 = new S3Client(); //람다에서 실행하면 자동으로 키를 넣어줌
 
@@ -12,17 +12,22 @@ exports.handler = async (event, context, callback) => {
     console.log('filename: ', filename, ' ext: ', ext);
 
     try {
-        const s3Object = await s3.getObject({ Bucket, Key });
-        console.log('original: ', s3Object.Body.length); //원본 파일 크기
-        const resizedImage = await sharp(s3Object.Body)
+        const getObject = await s3.send(new GetObjectCommand({ Bucket, Key }));
+        const buffers = [];
+        for await (const data of getObject.Body){
+            buffers.push(data);
+        }
+        const imageBuffer = Buffer.concat(buffers);
+        console.log('put: ', imageBuffer.length);
+        const resizedImage = await sharp(imageBuffer)
             .resize(200, 200, { fit: 'inside' })
             .toFormat(requiredFormat)
             .toBuffer();
-        await s3.putObject({
+        await s3.send(new PutObjectCommand({
             Bucket,
             Key: `thumb/${filename}`, 
-            Body: resizedImage
-        });
+            Body: resizedImage,
+        }));
         console.log('put: ', resizedImage.length); //압축 파일 크기
         return callback(null, `thumb/${filename}`); //callback 호출 시 코드의 흐름이 끝났다는 것을 알려줌
     } catch (error) {
